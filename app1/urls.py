3857f465967013e1544437d51b2b0ab85cb57b49
류인석 <ris2131>
2021-09-02 14:37:30 +0900

    made Post

--- app1/urls.py
+++ app1/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import helloAPI, randomUserInfo #내가 만든 함수
+from .views import helloAPI, randomUserInfo,inputUserInfo #내가 만든 함수
 
 urlpatterns =[
     path("hello/", helloAPI),
     path("<int:id>/", randomUserInfo),
+    path("inputUserInfo/",inputUserInfo),
 ]
 
