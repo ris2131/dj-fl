6c7dbf571026974860a30f652590eb7ae046fb91
류인석 <ris2131>
2021-09-03 18:39:58 +0900

    added model feedInfo

--- app1/admin.py
+++ app1/admin.py
@@ -3,5 +3,5 @@ from .models import userInfo,feedInfo
 # Register your models here.
 
 #관리자 페이지에 app1 모델을 register 하여 관리 할수 있도록 처리.
-#admin.site.register(userInfo)
+admin.site.register(userInfo)
 admin.site.register(feedInfo)
