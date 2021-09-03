aa74e51ee5dd4ad2aac8f08e55f6ad726f41492f
류인석 <ris2131>
2021-09-03 17:09:16 +0900

    added model feedInfo

--- app1/models.py
+++ app1/models.py
@@ -12,9 +12,8 @@ class userInfo(models.Model):
 class feedInfo(models.Model):
     feedID = models.AutoField() # primary key
     species = models.IntegerField() # 1:dog 2:cat # default=-1?
-    brand = models.charField(max_length=20,default = "no brand info")
-    feedName = models.charField(max_length=20,defaul = "no feedName info")
+    brand = models.CharField(max_length=20,default = "no brand info")
+    feedName = models.CharField(max_length=20,defaul = "no feedName info")
     price = models.IntegerField()#default =-1
     #allergy = #리스트 어떻게? #일단 charField받아올까? #아마 새로운 테이블(allergy) 만들어서 feedID 랑 엮어야하지 않을까 함.
     #flavor() = #리스트 어떻게?#일단 charField 받아올까? #아마 새로운 테이블 만들어서 feedID 랑 엮어야 하지 않을까 함
-    
