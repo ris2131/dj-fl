6a6d438ead1982f19a2606322b352d4306e08e03
류인석 <ris2131>
2021-09-09 17:51:22 +0900

    clear models feedInfo

--- app1/models.py
+++ app1/models.py
@@ -14,10 +14,10 @@ class feedInfo(models.Model):
     #id
     #id = models.AutoField(primary_key=True)
     #feedID = models.IntegerField(primary_key=True,auto_created=True) # primary key
-    species = models.IntegerField(default =0) # 1:dog 2:cat # default=-1?
+    species = models.IntegerField() # 1:dog 2:cat # default=-1?
     brand = models.CharField(max_length=20,default = "no brand info")
     feedName = models.CharField(max_length=20,default = "no feedName info")
-    price = models.IntegerField(default = -1)#default =-1
+    price = models.IntegerField()#default =-1
     #allergy = #리스트 어떻게? #일단 charField받아올까? #아마 새로운 테이블(allergy) 만들어서 feedID 랑 엮어야하지 않을까 함.
     #flavor() = #리스트 어떻게?#일단 charField 받아올까? #아마 새로운 테이블 만들어서 feedID 랑 엮어야 하지 않을까 함
     #pet = models.CharField(max_length=10,default="no pet info")
