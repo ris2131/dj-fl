cc8c491fdef20a76f267f815cc38e9e69a83aa19
류인석 <ris2131>
2021-09-09 14:56:26 +0900

    made getUserInfo

--- app1/models.py
+++ app1/models.py
@@ -12,8 +12,8 @@ class userInfo(models.Model):
 
 class feedInfo(models.Model):
     #id
-    id = models.AutoField(primary_key=True)
-    #feedID = models.IntegerField(primary_key=True,auto_created=True) # primary key
+    #id = models.AutoField(primary_key=True)
+    feedID = models.IntegerField(primary_key=True,auto_created=True) # primary key
     species = models.IntegerField() # 1:dog 2:cat # default=-1?
     brand = models.CharField(max_length=20,default = "no brand info")
     feedName = models.CharField(max_length=20,default = "no feedName info")
