66df6059bc69c0bf860139b68ddbb840ce235ef7
류인석 <ris2131>
2021-09-13 01:05:48 +0900

    modify csvToDict.py, view.py

--- app1/csvRelated/filterDictList.py
+++ app1/csvRelated/filterDictList.py
@@ -1,4 +1,5 @@
 
+"""
 def filter_flavor(filter_data, key):
     filtered_data = []
     
@@ -11,7 +12,19 @@ def filter_flavor(filter_data, key):
             data["cnt"] = cnt
             filtered_data.append(data)
     return filtered_data
-
+"""
+def filter_flavor(filter_data, key):
+    filtered_data = []
+    
+    for data in filter_data:
+        cnt = 0
+        for flavor_data in data["flavor"]:
+            if flavor_data in key:
+                cnt+=1
+        if cnt>0 or key[0] == [""]:
+            data["cnt"] = cnt
+            filtered_data.append(data)
+    return filtered_data
 def filter_alg(filter_data, key):
     filtered_data = []
     for data in filter_data:
@@ -28,14 +41,14 @@ def filter_alg(filter_data, key):
 def filter_health(filter_data, key):
     filtered_data = []
 
-    for data in filter : 
+    for data in filter_data : 
         flag = False
         for key_data in key:
             if key_data in data["health"]:
                 flag = True
         if flag == True:
             filtered_data.append(data)
-    return data
+    return filtered_data
 
 
 
