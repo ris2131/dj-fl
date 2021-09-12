30b1948009fda458f08fde4c5a95b412ee85768e
류인석 <ris2131>
2021-09-13 00:52:54 +0900

    modify findFeed

--- app1/views.py
+++ app1/views.py
@@ -64,7 +64,7 @@ def findFeed(request):
     #request.POST([])
     dict_list = csvToDictList(flavorKey,algKey,healthKey)
 
-    #filterDictList.filter_flavor(dict_list,flavorKey)
+    filterDictList.filter_flavor(dict_list,flavorKey)
     #filterDictList.filter_alg(dict_list,algKey)
     #filterDictList.filter_health(dict_list,healthKey)
 
