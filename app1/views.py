76f789009588724700b49720ae357528f0b33f2c
류인석 <ris2131>
2021-09-13 00:39:35 +0900

    modify findFeed

--- app1/views.py
+++ app1/views.py
@@ -55,7 +55,7 @@ def inputUserInfo(request):
 def findFeed(request):
     #alg, flavor, health [String]
     #algKey,flavorKey,healthKey
-    flavorKey = request.POST['flavor']
+    flavorKey = list(request.POST['flavor'])
     algKey = request.POST['alg']
     healthKey = request.POST['health']
     
